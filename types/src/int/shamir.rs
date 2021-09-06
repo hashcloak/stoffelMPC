0d5641ce80e6259979736c7c15619bb3f7458404
th4s <th4s@metavoid.xyz>
2021-09-06 19:32:50 +0200

    Some minor improvements
    - removed ark-ff-macros dependency
    - added some more tests for ShamirSecret
    - added display impl for ShamirSecret over PrimeField
    - improved generic layout for ShamirSecret

--- types/src/int/shamir.rs
+++ types/src/int/shamir.rs
@@ -2,15 +2,15 @@ use super::Open;
 use super::PubInt;
 use ark_bls12_381::Fr;
 use ark_ff::BigInteger256;
-use ark_ff::PrimeField;
+use ark_ff::{Field, PrimeField};
 use std::io::{Read, Write};
 use std::ops::{Add, Mul};
 use thiserror::Error;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
-pub(crate) struct ShamirSecret<T: PrimeField>(T);
+pub(crate) struct ShamirSecret<T: Field>(T);
 
-impl ShamirSecret<Fr> {
+impl<T: PrimeField> ShamirSecret<T> {
     pub fn new(element: BigInteger256) -> Result<ShamirSecret<Fr>, ShamirError> {
         let field_element =
             PrimeField::from_repr(element).ok_or(ShamirError::InitError(element))?;
@@ -18,7 +18,7 @@ impl ShamirSecret<Fr> {
     }
 }
 
-impl<T: PrimeField> Open for ShamirSecret<T> {
+impl<T: Field> Open for ShamirSecret<T> {
     type Public = PubInt;
 
     fn open<U: Read + Write>(self, channel: &mut U) -> Self::Public {
@@ -26,7 +26,7 @@ impl<T: PrimeField> Open for ShamirSecret<T> {
     }
 }
 
-impl<T: PrimeField> Add for ShamirSecret<T> {
+impl<T: Field> Add for ShamirSecret<T> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -34,7 +34,7 @@ impl<T: PrimeField> Add for ShamirSecret<T> {
     }
 }
 
-impl<T: PrimeField> Mul for ShamirSecret<T> {
+impl<T: Field> Mul for ShamirSecret<T> {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
@@ -42,9 +42,9 @@ impl<T: PrimeField> Mul for ShamirSecret<T> {
     }
 }
 
-impl std::fmt::Display for ShamirSecret<Fr> {
+impl<T: PrimeField> std::fmt::Display for ShamirSecret<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+        write!(f, "{}", T::into_repr(&self.0))
     }
 }
 
@@ -83,6 +83,15 @@ mod tests {
         assert_eq!(
             shamir_1 * shamir_2,
             ShamirSecret::<Fr>::new(210.into()).unwrap()
+        );
+    }
+
+    #[test]
+    fn shamir_display() {
+        let shamir_1 = ShamirSecret::<Fr>::new(42.into()).unwrap();
+        assert_eq!(
+            shamir_1.to_string(),
+            "000000000000000000000000000000000000000000000000000000000000002A"
         );
     }
 }
