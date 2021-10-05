2f1df735bf1ae2e8384ab69fde910ea6a78871a4
th4s <th4s@metavoid.xyz>
2021-10-05 17:06:15 +0200

    Added documentation for utils.rs

--- mpc/src/utils.rs
+++ mpc/src/utils.rs
@@ -7,22 +7,34 @@ mod polynomial {
     use ark_poly::{Polynomial as ArkPolynomial, UVPolynomial};
     use rand::thread_rng;
 
+    /// A struct representing a polynomial
+    ///
+    /// This struct wraps the `DensePolynomial` to provide
+    /// some utility functions.
     pub struct Polynomial<T: Field>(DensePolynomial<T>);
 
     impl<T: Field> Polynomial<T> {
+        /// Create a new polynomial from field elements.
+        ///
+        /// Uses a slice of field elements `coeffs` for creating
+        /// the polynomial, i.e. the coefficients. First coefficient
+        /// is for degree 0, second for degree 1, and so on.
         pub fn new(coeffs: &[T]) -> Self {
             Polynomial(DensePolynomial::from_coefficients_slice(coeffs))
         }
 
+        /// Creates a random polynomial of `degree`
         pub fn random(degree: usize) -> Self {
             let mut rng = thread_rng();
             Polynomial(DensePolynomial::rand(degree, &mut rng))
         }
 
-        pub fn set_coeff(&mut self, nth: usize, coefficient: T) {
-            self.0.coeffs[nth] = coefficient;
+        /// Sets the nth coefficient to `value`
+        pub fn set_coeff(&mut self, nth: usize, value: T) {
+            self.0.coeffs[nth] = value;
         }
 
+        /// Evaluates the polynomial at field element `value`
         pub fn evaluate(&self, value: &T) -> T {
             self.0.evaluate(value)
         }
