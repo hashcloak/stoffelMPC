22f951435569f7234068493eac31617433b63697
Mikerah <mikerah@hashcloak.com>
2021-10-24 12:04:59 -0400

    Skeleton code for register.rs

---
+++ vm/src/register.rs
@@ -0,0 +1,39 @@
+use types;
+
+pub Trait Register {
+
+}
+
+
+impl Register for  types::cint {
+    
+}
+
+impl Register for types::cfloat {
+    
+}
+
+impl Register for types::cgf2n {
+
+}
+
+impl Register for types::cbit {
+
+}
+
+impl Register for types::sint {
+
+}
+
+impl Register for types::sfloat {
+
+}
+
+impl Register for types::sgf2n {
+
+}
+
+impl Register for types::sbit {
+
+}
+
