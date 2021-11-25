03a97315165c3c2c2ac3662fbb1cbb30787794d6
Mikerah <mikerah@hashcloak.com>
2021-11-25 11:57:17 -0500

    Type generalization (#12)
    * Added gate trait and gate enums for each kind of circuit
    
    * Added basic skeleton for circuit implementation
    
    * Removed register.rs due to architecture changes
    
    * Added MPCType trait
    
    * Added MPCType to int.rs
    
    * Added MPCType to gf2.rs
    
    * Added MPCType to float.rs
    
    * Added MPCType to fixed.rs
    
    * Added MPCType to bit.rs
    
    * Small bug fixes
    
    - removed types/src/circuit.rs
    - removed inclusion of mod registers in vm/src/main.rs
    
    Co-authored-by: th4s <th4s@metavoid.xyz>

--- types/src/numbers/bit.rs
+++ types/src/numbers/bit.rs
@@ -1,4 +1,6 @@
 use super::secret_sharing::SecretSharing;
+use super::{MPCType};
+use std::ops::{Add, Mul};
 
 /// Public bit type
 ///
@@ -6,9 +8,93 @@ use super::secret_sharing::SecretSharing;
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct PubBit(bool);
 
+impl MPCType for PubBit { 
+    fn square(self) -> Self {
+        todo!();
+    }
+
+    fn pow(self, exp: usize) -> Self {
+        todo!();
+    }
+
+    fn min(self, a: Self) -> Self {
+        todo!();
+    }
+
+    fn max(self, a: Self) -> Self {
+        todo!();
+    }
+
+    fn if_else(self, a: Self, b: Self) -> Self {
+        todo!();
+    }
+
+    fn cond_swap(self, a: Self, b: Self) -> (Self, Self) {
+        todo!();
+    }
+}
+
+impl Add for PubBit {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        todo!();
+    }
+}
+
+impl Mul for PubBit {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        todo!();
+    }
+}
+
 /// Secret bit type
 ///
 /// This type wraps different implementations for secret bit types
 /// in order to provide a stable API for every type it wraps.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct SecBit<T: SecretSharing>(T);
+
+impl<T: SecretSharing> MPCType for SecBit<T> {
+    fn square(self) -> Self {
+        todo!();
+    }
+
+    fn pow(self, exp: usize) -> Self {
+        todo!();
+    }
+
+    fn min(self, a: Self) -> Self {
+        todo!();
+    }
+
+    fn max(self, a: Self) -> Self {
+        todo!();
+    }
+
+    fn if_else(self, a: Self, b: Self) -> Self {
+        todo!();
+    }
+
+    fn cond_swap(self, a: Self, b: Self) -> (Self, Self) {
+        todo!();
+    }
+}
+
+impl<T: SecretSharing> Add for SecBit<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        todo!();
+    }
+}
+
+impl<T: SecretSharing> Mul for SecBit<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        todo!();
+    }
+}
--- types/src/numbers/gf2.rs
+++ types/src/numbers/gf2.rs
@@ -1,4 +1,6 @@
 use super::secret_sharing::SecretSharing;
+use super::{MPCType};
+use std::ops::{Add, Mul};
 
 /// Public Gf2 type
 ///
@@ -6,9 +8,93 @@ use super::secret_sharing::SecretSharing;
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct PubGf2<const N: usize>([bool; N]);
 
+impl<const N: usize> MPCType for PubGf2<N> {
+    fn square(self) -> Self {
+        todo!();
+    }
+
+    fn pow(self, exp: usize) -> Self {
+        todo!();
+    }
+
+    fn min(self, a: Self) -> Self {
+        todo!();
+    }
+
+    fn max(self, a: Self) -> Self {
+        todo!();
+    }
+
+    fn if_else(self, a: Self, b: Self) -> Self {
+        todo!();
+    }
+
+    fn cond_swap(self, a: Self, b: Self) -> (Self, Self) {
+        todo!();
+    }
+}
+
+impl<const N: usize> Add for PubGf2<N> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        todo!();
+    }
+}
+
+impl<const N: usize> Mul for PubGf2<N> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        todo!();
+    }
+}
+
 /// Secret Gf2 type
 ///
 /// This type wraps different implementation for secret Gf2 types
 /// in order to provide a stable API for every type it wraps.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct SecGf2<T: SecretSharing>(T);
+
+impl<T: SecretSharing> MPCType for SecGf2<T> {
+    fn square(self) -> Self {
+        todo!();
+    }
+
+    fn pow(self, exp: usize) -> Self {
+        todo!();
+    }
+
+    fn min(self, a: Self) -> Self {
+        todo!();
+    }
+
+    fn max(self, a: Self) -> Self {
+        todo!();
+    }
+
+    fn if_else(self, a: Self, b: Self) -> Self {
+        todo!();
+    }
+
+    fn cond_swap(self, a: Self, b: Self) -> (Self, Self) {
+        todo!();
+    }
+}
+
+impl<T: SecretSharing> Add for SecGf2<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        todo!();
+    }
+}
+
+impl<T: SecretSharing> Mul for SecGf2<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        todo!();
+    }
+}
