7fe2b6c882a86e0614bb9819d9dd328e6898270c
th4s <th4s@metavoid.xyz>
2021-11-30 13:08:13 +0100

    WIP: added boolean and arithmetic gates and candidate for circuit trait

---
+++ types/src/circuit/boolean.rs
@@ -0,0 +1,41 @@
+use super::Wire;
+
+/// A gate type for boolean operations
+pub struct BooleanGate<T> {
+    first: Wire,
+    second: Wire,
+    operation: fn(x: T, y: T) -> T,
+}
+
+impl<T: std::ops::BitOr<Output = T>> BooleanGate<T> {
+    /// Create a gate for or
+    pub fn or(first: Wire, second: Wire) -> BooleanGate<T> {
+        BooleanGate {
+            first,
+            second,
+            operation: |first: T, second: T| -> T { first | second },
+        }
+    }
+}
+
+impl<T: std::ops::BitAnd<Output = T>> BooleanGate<T> {
+    /// Create a gate for and
+    pub fn add(first: Wire, second: Wire) -> BooleanGate<T> {
+        BooleanGate {
+            first,
+            second,
+            operation: |first: T, second: T| -> T { first & second },
+        }
+    }
+}
+
+impl<T: std::ops::Not<Output = T>> BooleanGate<T> {
+    /// Create a gate for not
+    pub fn not(first: Wire, second: Wire) -> BooleanGate<T> {
+        BooleanGate {
+            first,
+            second,
+            operation: |first: T, second: T| -> T { !first },
+        }
+    }
+}
