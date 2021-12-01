e956f8c91d31681b4787e9307c6f3b688b5ab0cb
th4s <th4s@metavoid.xyz>
2021-12-01 19:44:22 +0100

    WIP: Trying out different APIs for circuits

--- types/src/circuit/mod.rs
+++ types/src/circuit/mod.rs
@@ -4,12 +4,24 @@ mod boolean;
 #[derive(Debug, Clone, Copy)]
 pub struct Wire(usize);
 
-trait Circuit {
+trait Circuit<T> {
     type Gate;
     fn gates(&self) -> &[Self::Gate];
-    fn mut_gates(&mut self) -> &mut [Self::Gate];
-    fn nth_gate(&self, n: usize) -> &Self::Gate;
-    fn mut_nth_gate(&mut self, n: usize) -> &mut Self::Gate;
-    fn execute(&self) -> Self::Gate;
-    fn size(&self) -> usize;
+    fn gates_mut(&mut self) -> &mut [Self::Gate];
+
+    fn nth_gate(&self, n: usize) -> &Self::Gate {
+        &self.gates()[n]
+    }
+
+    fn nth_gate_mut(&mut self, n: usize) -> &mut Self::Gate {
+        &mut self.gates_mut()[n]
+    }
+
+    fn execute(&self) -> T {
+        todo!()
+    }
+
+    fn size(&self) -> usize {
+        self.gates().len()
+    }
 }
