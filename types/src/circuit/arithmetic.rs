355cfb0342e48143c1af03e6f0d1520be1fb0e4a
th4s <th4s@metavoid.xyz>
2021-12-07 18:43:59 +0100

    Removed div, sub methods in arithmetic and generalized to n inputs gates

--- types/src/circuit/arithmetic.rs
+++ types/src/circuit/arithmetic.rs
@@ -29,70 +29,59 @@ impl<T: Copy + Default> Circuit<T> for ArithmeticCircuit<T> {
         let gates_len = self.gates().len();
         let mut outputs: Vec<T> = vec![T::default(); inputs_len + gates_len];
         outputs[..inputs_len].copy_from_slice(&self.inputs);
-        for i in inputs_len..inputs_len + gates_len {
-            let current_gate = self.nth_gate(i);
-            let wire_1 = current_gate.first;
-            let wire_2 = current_gate.second;
-            outputs[inputs_len + i] = current_gate.execute(outputs[wire_1], outputs[wire_2])
-        }
-        outputs.remove(outputs.len() - 1)
+        for i in inputs_len..inputs_len + gates_len {}
+        outputs.remove(outputs.len() - 1);
+        todo!()
+    }
+
+    fn stats(&self) {
+        todo!()
     }
 }
 
 /// A gate type for arithmetic operations
-pub struct ArithmeticGate<T> {
-    pub first: usize,
-    pub second: usize,
-    operation: fn(x: T, y: T) -> T,
+pub struct ArithmeticGate<T: Copy + Default> {
+    pub inputs: Vec<usize>,
+    operation: fn(values: &[T]) -> T,
 }
 
-impl<T> ArithmeticGate<T> {
+impl<T: Copy + Default> ArithmeticGate<T> {
     // Execute the gate, yielding a result of the operation
-    pub fn execute(&self, x: T, y: T) -> T {
-        (self.operation)(x, y)
+    pub fn execute(&self, values: &[T]) -> T {
+        (self.operation)(values)
     }
 }
 
-impl<T: std::ops::Add<Output = T>> ArithmeticGate<T> {
+impl<T: Copy + Default + std::ops::Add<Output = T>> ArithmeticGate<T> {
     /// Create a gate for addition
-    pub fn add(first: usize, second: usize) -> ArithmeticGate<T> {
+    pub fn add(wires: &[usize]) -> ArithmeticGate<T> {
+        let inputs = wires.to_vec();
         ArithmeticGate {
-            first,
-            second,
-            operation: |x: T, y: T| -> T { x + y },
+            inputs,
+            operation: |values: &[T]| -> T {
+                let mut out = values[0];
+                for i in 1..values.len() {
+                    out = out + values[i]
+                }
+                out
+            },
         }
     }
 }
 
-impl<T: std::ops::Mul<Output = T>> ArithmeticGate<T> {
+impl<T: Copy + Default + std::ops::Mul<Output = T>> ArithmeticGate<T> {
     /// Create a gate for multiplication
-    pub fn mul(first: usize, second: usize) -> ArithmeticGate<T> {
+    pub fn mul(wires: &[usize]) -> ArithmeticGate<T> {
+        let inputs = wires.to_vec();
         ArithmeticGate {
-            first,
-            second,
-            operation: |x: T, y: T| -> T { x * y },
-        }
-    }
-}
-
-impl<T: std::ops::Div<Output = T>> ArithmeticGate<T> {
-    /// Create a gate for division
-    pub fn div(first: usize, second: usize) -> ArithmeticGate<T> {
-        ArithmeticGate {
-            first,
-            second,
-            operation: |x: T, y: T| -> T { x / y },
-        }
-    }
-}
-
-impl<T: std::ops::Sub<Output = T>> ArithmeticGate<T> {
-    /// Create a gate for subtraction
-    pub fn sub(first: usize, second: usize) -> ArithmeticGate<T> {
-        ArithmeticGate {
-            first,
-            second,
-            operation: |x: T, y: T| -> T { x - y },
+            inputs,
+            operation: |values: &[T]| -> T {
+                let mut out = values[0];
+                for i in 1..values.len() {
+                    out = out * values[i]
+                }
+                out
+            },
         }
     }
 }
