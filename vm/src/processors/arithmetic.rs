00890e131457bf14b3eaf41f9fa5c44abe97f83e
Mikerah <mikerah@hashcloak.com>
2021-12-29 19:41:07 -0500

    Added basic arithmetic and boolean processors

--- vm/src/processors/arithmetic.rs
+++ vm/src/processors/arithmetic.rs
@@ -1,9 +1,64 @@
-use super::{Processor};
+use super::{Processor, Register, StackRegister};
+use types::numbers::{MPCType};
 
-struct ArithmeticProcessor {
+pub struct ArithmeticProcessor {
+    // Registers
+    reg_cint_mod_p: Register,
+    reg_register_int: Register,
+    reg_sint_mod_p: Register,
+    reg_sregister_int: Register,
+
+    // Stacks
+    stack_cint_mod_p: StackRegister,
+    stack_register_int: StackRegister,
+    reg_sint_mod_p: StackRegister,
+    reg_sregister_int: StackRegister
 
 }
 
 impl Processor for ArithmeticProcessor {
+    fn clear_registers() {
+        todo!();
+    }
+
+    fn get_program_counter() -> uint {
+        todo!();
+    }
+
+    fn get_program_size() -> uint {
+        todo!();
+    }
+
+    fn execute() {
+        todo!();
+    }
+    fn jump() {
+        todo!();
+    }
+
+    fn relative_jump() {
+        todo!();
+    }
+
+    fn increment_program_counter() {
+        todo!();
+    }
+
+    fn read_tape() {
+        todo!();
+    }
+    fn receive_tape() {
+        todo!();
+    }
+    fn receive_private_input(to_store_in_memory: bool) {
+        todo!();
+    }
+    fn give_private_output(to_store_in_memory: bool) {
+        todo!()
+    }
+
+}
+
+impl ArithmeticProcessor {
 
 }
--- vm/src/processors/boolean.rs
+++ vm/src/processors/boolean.rs
@@ -1,10 +1,63 @@
-use super::{Processor};
+use super::{Processor, Register, StackRegister};
+use types::numbers::{MPCType};
 
+pub struct BooleanProcessor {
+    // Registers
+    reg_cgf2: Register,
+    reg_sgf2: Register,
+    reg_cgf2n: Register,
+    reg_sgf2n: Register,
 
-struct BooleanProcessor {
-
+    // Stack Registers
+    stack_cgf2: StackRegister,
+    stack_sgf2: StackRegister,
+    stack_cgf2n: StackRegister,
+    stack_sgf2n: StackRegister
 }
 
 impl Processor for BooleanProcessor {
+    fn clear_registers() {
+        todo!();
+    }
+
+    fn get_program_counter() -> uint {
+        todo!();
+    }
+
+    fn get_program_size() -> uint {
+        todo!();
+    }
+
+    fn execute() {
+        todo!();
+    }
+    fn jump() {
+        todo!();
+    }
+
+    fn relative_jump() {
+        todo!();
+    }
+
+    fn increment_program_counter() {
+        todo!();
+    }
+
+    fn read_tape() {
+        todo!();
+    }
+    fn receive_tape() {
+        todo!();
+    }
+    fn receive_private_input(to_store_in_memory: bool) {
+        todo!();
+    }
+    fn give_private_output(to_store_in_memory: bool) {
+        todo!()
+    }
+
+}
+
+impl BooleanProcessor {
 
 }
