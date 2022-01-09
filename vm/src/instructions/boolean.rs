f6d08ebcc7fa5e9cbe3619d092b4b45d6d64ea6a
Mikerah <mikerah@hashcloak.com>
2022-01-09 12:27:34 -0500

    Switched instructions to snake case

--- vm/src/instructions/boolean.rs
+++ vm/src/instructions/boolean.rs
@@ -2,242 +2,242 @@ use crate::processor::boolean::BooleanProcessor;
 use types::numbers::MPCType;
 
 // Copy private input to secret bit register vectors
-pub fn INPUTB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn inputb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy private input secret bit registers bit by bit
-pub fn INPUTBVEC<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn inputbvec<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
-pub fn LDMSD<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn ldmsd<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
-pub fn STMSD<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn stmsd<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
-pub fn LDMSDI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn ldmsdi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
-pub fn STMSDI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn stmsdi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
-pub fn STMSDCI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn stmsdci<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Bitwise XOR of secret bit register
-pub fn XORS<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn xors<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Bitwise XOR of two single clear bit registers
-pub fn XORCB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn xorcb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Bitwise XOR of single clear bit register and immediate
-pub fn XORCBI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn xorcbi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Bitwise NOT of secret register vector
-pub fn NOTS<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn nots<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Bitwise NOT of secret register vector
-pub fn NOTCB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn notcb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Constant-vector AND of secret bit registeers
-pub fn ANDRS<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn andrs<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Bitwise AND of secret bit register vector
-pub fn ANDS<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn ands<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Integer addition two single clear bit registers
-pub fn ADDCB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn addcb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Integer addition single clear bit register and immediate
-pub fn ADDCBI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn addcbi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Integer multiplication single clear bit register and immediate
-pub fn MULCBI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn mulcbi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Secret bit register decomposition
-pub fn BITDECS<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn bitdecs<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Secret bit register decomposition
-pub fn BITCOMS<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn bitcoms<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Secret bit register decomposition
-pub fn BITDECC<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn bitdecc<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Right shift of clear bit register by immediate
-pub fn SHRCBI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn shrcbi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Left shift of clear bit register by immediate
-pub fn SHLCBI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn shlcbi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Store immediate in secret bit register
-pub fn LDBITS<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn ldbits<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy secret bit memory cell with compile-time address to secret bit register
-pub fn LDMSB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn ldmsb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy secret bit register to secret bit memory cell with compile-time address
-pub fn STMSB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn stmsb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy secret bit memory cell with run-time address to secret bit register
-pub fn LDMSBI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn ldmsbi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy secret bit register to secret bit memory cell with run-time address
-pub fn STMSBI<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn stmsbi<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy clear bit memory cell with compile-time address to clear bit register
-pub fn LDMCB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn ldmcb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy clear bit register to cleare bit memory cell with compile-time address
-pub fn STMCB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn stmcb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
-pub fn MOVSB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn movsb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Secret bit register vector transpose
-pub fn TRANS<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn trans<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy fresh secret random bit to secret bit register
-pub fn BITB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn bitb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Reveal secret bit register vectors and copy result to clear bit register vectors
-pub fn REVEAL<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn reveal<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Signed output of clear bit register
-pub fn PRINTREGSIGNED<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn printregsigned<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Debug output of clear bit register
-pub fn PRINTREGB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn printregb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Output clear bit register
-pub fn PRINTREGPLAINB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn printregplainb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Output floating-number from clear bit registers
-pub fn PRINTFLOATPLAINB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn printfloatplainb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Conditionally output four bytes
-pub fn CONDPRINTSTRB<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn condprintstrb<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Bitwise AND of single secret and clear bit registers
-pub fn ANDM<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn andm<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy clear integer register to secret bit register
-pub fn CONVSINT<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn convsint<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy clear integer register to clear bit register
-pub fn CONVCINT<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn convcint<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy clear register vector by bit to clear bit register vectors
-pub fn CONVCINTVEC<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn convcintvec<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy clear bit register to clear integer register
-pub fn CONVCBIT<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn convcbit<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
-pub fn CONVCBITVEC<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn convcbitvec<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Copy clear bit register vector to secret bit register vector
-pub fn CONVCBIT2S<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn convcbit2s<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Store fresh random daBits in secret register
 // @dev: Do we need to keep this?
-pub fn DABIT<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn dabit<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Store frehs random loose edaBits in secret register
 // @dev: Do we need to keep this?
-pub fn EDABIT<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn edabit<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Store fresh random strict edaBits in secret register
-pub fn SEDABIT<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn sedabit<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
 // Local share conversion
-pub fn SPLIT<T: MPCType>(bp: &mut BooleanProcessor<T>) {
+pub fn split<T: MPCType>(bp: &mut BooleanProcessor<T>) {
     todo!();
 }
 
--- vm/src/instructions/common.rs
+++ vm/src/instructions/common.rs
@@ -3,218 +3,218 @@
 use crate::processor::Processor;
 
 // Output a single byte
-pub fn PRINTCHR<P: Processor>(p: &mut P) {
+pub fn printchr<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Output four bytes
-pub fn PRINTSTR<P: Processor>(p: &mut P) {
+pub fn printstr<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Set number of digits after decimal point for PRINTFLOATPLAIN
 // @dev: Do we need this?
-pub fn PRINTFLOATPREC<P: Processor>(p: &mut P) {
+pub fn printfloatprec<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Store immediate value in clear integer register
-pub fn LDINT<P: Processor>(p: &mut P) {
+pub fn ldint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer register addition
-pub fn ADDINT<P: Processor>(p: &mut P) {
+pub fn addint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clera integer register subtraction
-pub fn SUBINT<P: Processor>(p: &mut P) {
+pub fn subint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer register multiplication
-pub fn MULINT<P: Processor>(p: &mut P) {
+pub fn mulint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer register division with floor rounding
-pub fn DIVINT<P: Processor>(p: &mut P) {
+pub fn divint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Unconditional relative jump in the bytecode
-pub fn JMP<P: Processor>(p: &mut P) {
+pub fn jmp<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Conditional relative jump in the bytecode
 // NZ = Not Zero
-pub fn JMPNZ<P: Processor>(p: &mut P) {
+pub fn jmpnz<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Conditional relative jump in the bytecode
-pub fn JMPEQZ<P: Processor>(p: &mut P) {
+pub fn jmpeqz<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer zero test
-pub fn EQZC<P: Processor>(p: &mut P) {
+pub fn eqzc<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer less than zero test
-pub fn LTZC<P: Processor>(p: &mut P) {
+pub fn ltzc<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer less than comparison
-pub fn LTC<P: Processor>(p: &mut P) {
+pub fn ltc<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer greater than comparison
-pub fn GTC<P: Processor>(p: &mut P) {
+pub fn gtc<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer equality test
-pub fn EQC<P: Processor>(p: &mut P) {
+pub fn eqc<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Unconditional relative jump in the bytecode
-pub fn JMPI<P: Processor>(p: &mut P) {
+pub fn jmpi<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Assign clear integer memory value(s) to clear integer register by immediate address
-pub fn LDMINT<P: Processor>(p: &mut P) {
+pub fn ldmint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Assign clear integer memory value(s) to clear integer register by immeiate address
-pub fn STMINT<P: Processor>(p: &mut P) {
+pub fn stmint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Assign clear integer memory value(s) to clear integer register by register address
-pub fn LDMINTI<P: Processor>(p: &mut P) {
+pub fn ldminti<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Assign clear integer to clear integer memory value(s) by register address
-pub fn STMINTI<P: Processor>(p: &mut P) {
+pub fn stminti<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Pushes clear integer register to the thread-local stack
-pub fn PUSHINT<P: Processor>(p: &mut P) {
+pub fn pushint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Pops from the thread-local stack to clear integer
-pub fn POPINT<P: Processor>(p: &mut P) {
+pub fn popint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Copy clear integer register
-pub fn MOVINT<P: Processor>(p: &mut P) {
+pub fn movint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Clear integer bit decomposition
-pub fn BITDECINT<P: Processor>(p: &mut P) {
+pub fn bitdecint<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Store the argument passed to the current thread in clear integer register
-pub fn LDARG<P: Processor>(p: &mut P) {
+pub fn ldarg<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Copy clear integer register to the thread argument
-pub fn STARG<P: Processor>(p: &mut P) {
+pub fn starg<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Output time since start of computation
-pub fn TIME<P: Processor>(p: &mut P) {
+pub fn time<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Start timer
-pub fn START<P: Processor>(p: &mut P) {
+pub fn start<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Stop timer
-pub fn STOP<P: Processor>(p: &mut P) {
+pub fn stop<P: Processor>(p: &mut P) {
     todo!();
 }
 
-pub fn GLDMS<P: Processor>(p: &mut P) {
+pub fn gldms<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Assign clear memory value(s) to clear register by immediate address
-pub fn LDMC<P: Processor>(p: &mut P) {
+pub fn ldmc<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Assign secret memory value(s) to secret register by immediate address
-pub fn LDMS<P: Processor>(p: &mut P) {
+pub fn ldms<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Output clear integer register
 // @dev: Should we keep this?
-pub fn PRINTINT<P: Processor>(p: &mut P) {
+pub fn printint<P: Processor>(p: &mut P) {
     todo!();
 }
 
-pub fn GLDMC<P: Processor>(p: &mut P) {
+pub fn gldmc<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Start tape/bytecode file in another thread
-pub fn RUN_TAPE<P: Processor>(p: &mut P) {
+pub fn run_tape<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Join thread
-pub fn JOIN_TAPE<P: Processor>(p: &mut P) {
+pub fn join_tape<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Crash runtime if the register's value is > 0
-pub fn CRASH<P: Processor>(p: &mut P) {
+pub fn crash<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Offline data usage
-pub fn USE<P: Processor>(p: &mut P) {
+pub fn use<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Input usage
-pub fn USE_INP<P: Processor>(p: &mut P) {
+pub fn use_inp<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Store number of players in clear integer register
-pub fn NPLAYERS<P: Processor>(p: &mut P) {
+pub fn nplayers<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Store maximal number of corrupt players in clear integer register
-pub fn THRESHOLD<P: Processor>(p: &mut P) {
+pub fn threshold<P: Processor>(p: &mut P) {
     todo!();
 }
 
 // Store current player number in clear integer register
-pub fn PLAYERID<P: Processor>(p: &mut P) {
+pub fn playerid<P: Processor>(p: &mut P) {
     todo!();
 }
 
