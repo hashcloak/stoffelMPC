a18addb83de1429db29bb27e18d94f01bbfc0c16
Mikerah <mikerah@hashcloak.com>
2022-01-18 21:38:08 -0500

    Basic API for StoffelVM

--- vm/src/stoffel_vm.rs
+++ vm/src/stoffel_vm.rs
@@ -1,3 +1,4 @@
+#![feature(associated_type_bounds)]
 use types::numbers::secret_sharing::SecretSharing;
 use types::numbers::{
     MPCType,
@@ -6,13 +7,100 @@ use types::numbers::{
     int::{PubInt, SecInt},
 };
 
-use super::processors::processor::{Processor};
+use super::processors::{Processor, arithmetic::ArithmeticProcessor, boolean::BooleanProcessor};
 use super::state::{Memory, Register, StackRegister};
 
-struct StoffelVM {
-   
+
+pub trait StoffelVM {
+    fn load_program();
+
+    fn execute();
+
+    fn load_schedule();
+
+    fn set_mode();
+
+    fn start_timer();
+
+    fn stop_timer();
+
+    fn get_current_program_counter();
 }
 
-impl StoffelVM {
+enum VMMode {
+    Normal,
+    Debug,
+    Optimized
+}
 
+pub struct ArithmeticStoffelVM<T: MPCType> {
+    processors: Vec<ArithmeticProcessor<T>>,
+    program_counter: usize,
+    mode: VMMode,
+}
+
+pub struct BooleanStoffelVM<T: MPCType> {
+    processors: Vec<BooleanProcessor<T>>,
+    program_counter: usize,
+    mode: VMMode,
+}
+
+impl<T: MPCType> StoffelVM for ArithmeticStoffelVM<T> {
+    fn load_program() {
+        todo!();
+    }
+
+    fn execute() {
+        todo!();
+    }
+
+    fn load_schedule() {
+        todo!();
+    }
+
+    fn set_mode() {
+        todo!();
+    }
+
+    fn start_timer() {
+        todo!();
+    }
+
+    fn stop_timer() {
+        todo!();
+    }
+
+    fn get_current_program_counter() {
+        todo!();
+    }
+}
+
+impl<T: MPCType> StoffelVM for BooleanStoffelVM<T> {
+    fn load_program() {
+        todo!();
+    }
+
+    fn execute() {
+        todo!();
+    }
+
+    fn load_schedule() {
+        todo!();
+    }
+
+    fn set_mode() {
+        todo!();
+    }
+
+    fn start_timer() {
+        todo!();
+    }
+
+    fn stop_timer() {
+        todo!();
+    }
+
+    fn get_current_program_counter() {
+        todo!();
+    }
 }
