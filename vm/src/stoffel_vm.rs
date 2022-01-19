9cb2bceb5c284e64bee3ec140b926ab1f47ede46
Mikerah <mikerah@hashcloak.com>
2022-01-18 21:08:24 -0500

    Basic ideas for StoffelVM APIs done

--- vm/src/stoffel_vm.rs
+++ vm/src/stoffel_vm.rs
@@ -6,13 +6,136 @@ use types::numbers::{
     int::{PubInt, SecInt},
 };
 
-use super::processors::processor::{Processor};
+use super::processors::{Processor, ArithmeticProcessor, BooleanProcessor};
 use super::state::{Memory, Register, StackRegister};
 
-struct StoffelVM {
-   
+
+pub trait StoffelVM {
+    fn load_program();
+
+    fn execute();
+
+    fn load_schedule();
+
+    fn set_mode();
+
+    fn start_timer();
+
+    fn stop_timer();
+
+    fn get_current_program_counter();
 }
 
-impl StoffelVM {
+enum VMMode {
+    Normal,
+    Debug,
+    Optimized
+}
 
+pub struct StoffelVM<P: Processor> {
+    processors: Vec<P>,
+    program_counter: usize,
+    mode: VMMode
+}
+
+pub struct ArithmeticStoffelVM {
+    processors: Vec<ArithmeticProcessor>,
+    program_counter: usize,
+    mode: VMMode,
+}
+
+pub struct BooleanStoffelVM {
+    processors: Vec<BooleanProcessor>,
+    program_counter: usize,
+    mode: VMMode,
+}
+
+impl StoffelVM<P: Processor> for StoffelVM<P> {
+    fn load_program() {
+        todo!();
+    }
+
+    fn execute() {
+        todo!();
+    }
+
+    fn load_schedule() {
+        todo!();
+    }
+
+    fn set_mode() {
+        todo!();
+    }
+
+    fn start_timer() {
+        todo!();
+    }
+
+    fn stop_timer() {
+        todo!();
+    }
+
+    fn get_current_program_counter() {
+        todo!();
+    }
+}
+
+impl StoffelVM for ArithmeticStoffelVM {
+    fn load_program() {
+        todo!();
+    }
+
+    fn execute() {
+        todo!();
+    }
+
+    fn load_schedule() {
+        todo!();
+    }
+
+    fn set_mode() {
+        todo!();
+    }
+
+    fn start_timer() {
+        todo!();
+    }
+
+    fn stop_timer() {
+        todo!();
+    }
+
+    fn get_current_program_counter() {
+        todo!();
+    }
+}
+
+impl StoffelVM for BooleanStoffelVM {
+    fn load_program() {
+        todo!();
+    }
+
+    fn execute() {
+        todo!();
+    }
+
+    fn load_schedule() {
+        todo!();
+    }
+
+    fn set_mode() {
+        todo!();
+    }
+
+    fn start_timer() {
+        todo!();
+    }
+
+    fn stop_timer() {
+        todo!();
+    }
+
+    fn get_current_program_counter() {
+        todo!();
+    }
 }
