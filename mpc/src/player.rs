bcff00c70fd9a8b4733b521287867e29e16c989b
Mikerah <mikerah@hashcloak.com>
2022-02-01 10:08:07 -0500

    Update player.rs

--- mpc/src/player.rs
+++ mpc/src/player.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 use super::protocols::{MPCProtocol};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
@@ -7,24 +6,13 @@ pub struct Player {
     socket: SocketAddr,
     protocol: MPCProtocol,
     vm: StoffelVM,
-=======
-use vm::StoffelVM;
-use crate::mpc::protocol;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-
-pub struct Player<V: StoffelVM, M: MPCProtocol> {
-    id: str,
-    vm: V<M>,
-    socket: SocketAddr, 
->>>>>>> db8ffc1d3d47abc265f41aec28c6c3204b2d87fb
 }
 
 impl Player {
     fn new() -> Self {
         todo!();
     }
-
-<<<<<<< HEAD
+    
     fn send_to_player() -> Result<(), Box<dyn std::error::Error>> {
         todo!();
     }
@@ -34,18 +22,4 @@ impl Player {
     }
 
     fn broadcast_and_receive() -> Result<(), Box<dyn std::error::Error>> {
-=======
-    fn send_to_player() -> Result<()> {
-        todo!();
-    }
-
-    fn receive_from_player() -> Result<()> {
-        todo!();
-    }
-
-    fn broadcast_and_receive() -> Result<()> {
->>>>>>> db8ffc1d3d47abc265f41aec28c6c3204b2d87fb
-        todo!();
-    }
-
 }
