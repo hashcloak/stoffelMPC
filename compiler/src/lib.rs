5dbce83ace710ec4af8d9e42d38d2e072105c4c4
Mikerah <mikerah@hashcloak.com>
2022-06-11 19:21:35 -0400

    Reorganized compiler crate and added parser skeleton

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,1 +1,4 @@
+#[macro_use]
+extern crate pest_derive;
+
 pub mod parser;
--- compiler/src/parser/mod.rs
+++ compiler/src/parser/mod.rs
@@ -1,6 +1,4 @@
 extern crate pest;
-#[macro_use]
-extern crate pest_derive;
 
 use std::{fs, path::Path};    
 use pest::{Parser, iterators::Pairs, error::Error, Span};   
@@ -16,34 +14,82 @@ pub struct StoffelLangParser;
 
 #[derive(Clone, Debug, FromPest, PartialEq)]
 #[pest_ast(rule(Rule::program))]
-pub struct Program {};
+pub struct Program {}
 
 #[derive(Clone, Debug, FromPest, PartialEq)]
 #[pest_ast(rule(Rule::program_dec))]
-pub struct ProgramDec {};
+pub struct ProgramDec {}
 
 #[derive(Clone, Debug, FromPest, PartialEq)]
-#[pest_ast(rule(Rule::EOI)]
-pub struct EOI;
+#[pest_ast(rule(Rule::program_block))]
+pub struct ProgramBlock {}
 
 #[derive(Clone, Debug, FromPest, PartialEq)]
-#[pest_ast(rule(Rule::ident)]
-pub struct Ident;
+#[pest_ast(rule(Rule::non_variable_declarations))]
+pub struct NonVarDecs {}
 
 #[derive(Clone, Debug, FromPest, PartialEq)]
-#[pest_ast(rule(Rule::expr]
-pub struct Expr {};
+#[pest_ast(rule(Rule::EOI))]
+pub struct EOI {}
 
 #[derive(Clone, Debug, FromPest, PartialEq)]
-#[pest_ast(rule(Rule::expr_inner]
-pub struct ExprInner {};
+#[pest_ast(rule(Rule::ident))]
+pub struct Ident {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::expr))]
+pub struct Expr {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::expr_inner))]
+pub struct ExprInner {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::if_exp))]
+pub struct IfExpr {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::code_block))]
+pub struct CodeBlock {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::literal_value))]
+pub struct LiteralValue {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::parenthesized_expr))]
+pub struct ParenthesizedExpr {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::struct_dec))]
+pub struct StructDec {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::variable_dec))]
+pub struct VariableDec {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::storage_dec))]
+pub struct StorageDec {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::function_dec))]
+pub struct FunctionDec {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::while_loop))]
+pub struct WhileLoop {}
+
+#[derive(Clone, Debug, FromPest, PartialEq)]
+#[pest_ast(rule(Rule::for_loop))]
+pub struct ForLoop {}
 
 pub fn parse(input: &str) -> Result<Pairs<Rule>, Error<Rule>> {
-    StoffelLangParser::parse(Rule::program, input);
+    StoffelLangParser::parse(Rule::program, input)
 }
 
 pub fn span_into_string(span: Span) -> String {
-    span.as_str().to_string();
+    span.as_str().to_string()
 }
 
 impl StoffelLangParser {
