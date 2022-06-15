22c541390bbb1dad9ecadfc4c7878a68cd1c6dbc
Mikerah <mikerah@hashcloak.com>
2022-06-14 22:36:08 -0400

    Basic skeleton for CLI

---
+++ mwrc/src/cli/commands/test.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+/// Runs tests for a StoffelMPC project
+#[derive(Debug, Default, Parser)]
+pub struct Command {
+
+}
+
+pub(crate) fn exec(command: Command) -> Result<()> {
+    todo!();
+}
