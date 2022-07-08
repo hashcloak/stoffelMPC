fd5a534b356c5e3929d3049a956bc88611c53fe0
Mikerah <mikerah@hashcloak.com>
2022-07-08 18:19:01 -0400

    Reorganized the opcodes to use new vm types

--- vm/src/instructions/mod.rs
+++ vm/src/instructions/mod.rs
@@ -2,3 +2,23 @@ pub mod arithmetic;
 pub mod boolean;
 pub mod common;
 pub mod opcodes;
+
+use opcodes::{Opcode, InstructionOpcode};
+use super::processor::Processor;
+
+#[derive(Debug)]
+pub struct Instruction<P: Processor> {
+    opcode: Opcode,
+    opcode_name: InstructionOpcode,
+    processor: P,
+}
+
+impl<P: Processor> Instruction<P> {
+    pub fn new() -> Self {
+        todo!();
+    }
+
+    pub fn parse(&mut self) -> Result<(),Box<dyn std::error::Error>>{
+        todo!();
+    }
+}
--- vm/src/instructions/opcodes.rs
+++ vm/src/instructions/opcodes.rs
@@ -2,7 +2,10 @@
 /// These are the instructions for the StoffelMPC VM. These instructions are taken from the MP-SPDZ's VM instructions
 /// These instructions may be reduced or added to at a later time.
 
-#[derive(Debug, Clone, Copy)]
+use crate::state::{Register, Memory};
+use types::vm::{RegisterAddr, ImmediateValue};
+
+/// Not needed yet ;) #[derive(Debug, Clone, Copy)]
 pub type InstructionOpcode = u16;
 
 /// Load/Store Opcodes
@@ -18,15 +21,12 @@ pub const OP_STMCI: InstructionOpcode = 0x09;
 pub const OP_STMSI: InstructionOpcode = 0x0A;
 pub const OP_MOVC: InstructionOpcode = 0x0B;
 pub const OP_MOVS: InstructionOpcode = 0x0C;
-pub const OP_PROTECTMEMS: InstructionOpcode = 0x0D;
-pub const OP_PROTECTMEMC: InstructionOpcode = 0x0E;
-pub const OP_PROTECTMEMINT: InstructionOpcode = 0x0F;
 pub const OP_LDMINT: InstructionOpcode = 0xCA;
 pub const OP_STMINT: InstructionOpcode = 0xCB;
 pub const OP_LDMINTI: InstructionOpcode = 0xCC;
 pub const OP_STMINTI: InstructionOpcode = 0xCD;
 pub const OP_PUSHINT: InstructionOpcode = 0xCE;
-pub const OP_POPINT: InstructionOpcode = 0xCF
+pub const OP_POPINT: InstructionOpcode = 0xCF;
 pub const OP_MOVINT: InstructionOpcode = 0xD0;
 /// Machine Opcodes
 pub const OP_LDTN: InstructionOpcode = 0x10;
@@ -34,16 +34,13 @@ pub const OP_LDARG: InstructionOpcode = 0x11;
 pub const OP_REQBL: InstructionOpcode = 0x12;
 pub const OP_STARG: InstructionOpcode = 0x13;
 pub const OP_TIME: InstructionOpcode = 0x14;
-pub const OP_START: InstructionOpcode = 0x15;
-pub const OP_STOP: InstructionOpcode = 0x16;
+pub const OP_STARTTIMER: InstructionOpcode = 0x15;
+pub const OP_STOPTIMER: InstructionOpcode = 0x16;
 pub const OP_USE: InstructionOpcode = 0x17;
 pub const OP_USE_INP: InstructionOpcode = 0x18;
 pub const OP_USE_RUN_TAPE: InstructionOpcode = 0x19;
 pub const OP_JOIN_TAPE: InstructionOpcode = 0x1A;
 pub const OP_CRASH: InstructionOpcode = 0x1B;
-pub const OP_USE_PREP: InstructionOpcode = 0x1C;
-pub const OP_STARTGRIND: InstructionOpcode = 0x1D; // May not be necessary
-pub const OP_STOPGRIND: InstructionOpcode = 0x1E; // May not be necessary
 pub const OP_NPLAYERS: InstructionOpcode = 0xE2;
 pub const OP_THRESHOLD: InstructionOpcode = 0xE3;
 pub const OP_PLAYERID: InstructionOpcode = 0xE4;
@@ -78,25 +75,20 @@ pub const OP_FLOORDIVC: InstructionOpcode = 0x3B;
 pub const OP_GMULBITC: InstructionOpcode = 0x136;
 pub const OP_MULBITM: InstructionOpcode = 0x137;
 /// Open
-pub const OP_OPEN: InstructionOpcode = 0xA5;
 pub const OP_MULS: InstructionOpcode = 0xA6;
 pub const OP_MULRS: InstructionOpcode = 0xA7;
 pub const OP_DOTPRODS: InstructionOpcode = 0xA8;
 pub const OP_TRUNC_PR: InstructionOpcode = 0xA9;
 pub const OP_MATMULS: InstructionOpcode = 0xAA;
 pub const OP_MATMULSM: InstructionOpcode = 0xAB;
-pub const OP_CONV2DS: InstructionOpcode = 0xAC;
 pub const OP_CHECK: InstructionOpcode = 0xAF;
 /// Data access
 pub const OP_TRIPLE: InstructionOpcode = 0x50;
 pub const OP_BIT: InstructionOpcode = 0x51;
 pub const OP_SQUARE: InstructionOpcode = 0x52;
 pub const OP_INV: InstructionOpcode = 0x53;
-pub const OP_GBITTRIPLE: InstructionOpcode = 0x154;
-pub const OP_GBITGF2NTRIPLE: InstructionOpcode = 0x155;
 pub const OP_INPUTMASK: InstructionOpcode = 0x56;
 pub const OP_INPUTMASKREG: InstructionOpcode = 0x5C;
-pub const OP_PREP: InstructionOpcode = 0x57;
 pub const OP_DABIT: InstructionOpcode = 0x58;
 pub const OP_EDABIT: InstructionOpcode = 0x59;
 pub const OP_SEDABIT: InstructionOpcode = 0x5A;
@@ -190,188 +182,192 @@ pub const OP_RESPSECURESOCKET: InstructionOpcode = 0x1BB;
 #[derive(Debug, Clone, Copy)]
 pub enum Opcode {
     /// Emulation
-    CISC = 0x00,
+    // CISC = 0x00,
     /// Load/store
-    /// LDI(bytecode as a hex string, immediate value, register position)
-    LDI(InstructionOpcode, u64, usize),
-    LDSI = 0x02,
-    LDMC = 0x03,
-    LDMS = 0x04,
-    STMC = 0x05,
-    STMS = 0x06,
-    LDMCI = 0x07,
-    LDMSI = 0x08,
-    STMCI = 0x09,
-    STMSI = 0x0A,
-    MOVC = 0x0B,
-    MOVS = 0x0C,
-    PROTECTMEMS = 0x0D,
-    PROTECTMEMC = 0x0E,
-    PROTECTMEMINT = 0xF,
-    LDMINT = 0xCA,
-    STMINT = 0xCB,
-    LDMINTI = 0xCC,
-    STMINTI = 0xCD,
-    PUSHINT = 0xCE,
-    POPINT = 0xCF,
-    MOVINT = 0xD0,
+    /// (bytecode, immediate value, register/memory position)
+    LDI(InstructionOpcode, ImmediateValue, RegisterAddr),
+    LDSI(InstructionOpcode, ImmediateValue, RegisterAddr),
+    LDMC(InstructionOpcode, ImmediateValue, RegisterAddr),
+    LDMS(InstructionOpcode, ImmediateValue, RegisterAddr),
+    STMC(InstructionOpcode, ImmediateValue, RegisterAddr),
+    STMS(InstructionOpcode, ImmediateValue, RegisterAddr),
+    /// (bytecode, memory, register)
+    LDMCI(InstructionOpcode, RegisterAddr, RegisterAddr),
+    LDMSI(InstructionOpcode, RegisterAddr, RegisterAddr),
+    STMCI(InstructionOpcode, RegisterAddr, RegisterAddr),
+    STMSI(InstructionOpcode, RegisterAddr, RegisterAddr),
+    MOVC(InstructionOpcode, RegisterAddr, RegisterAddr),
+    MOVS(InstructionOpcode, RegisterAddr, RegisterAddr),
+    LDMINT(InstructionOpcode, ImmediateValue, RegisterAddr),
+    STMINT(InstructionOpcode, ImmediateValue, RegisterAddr),
+    LDMINTI(InstructionOpcode, RegisterAddr, RegisterAddr),
+    STMINTI(InstructionOpcode, RegisterAddr, RegisterAddr),
+    PUSHINT(InstructionOpcode, RegisterAddr),
+    POPINT(InstructionOpcode, RegisterAddr),
+    MOVINT(InstructionOpcode, RegisterAddr, RegisterAddr),
     /// Machine
-    LDTN = 0x10,
-    LDARG = 0x11,
-    REQBL = 0x12,
-    STARG = 0x13,
-    TIME = 0x14,
-    START = 0x15,
-    STOP = 0x16,
-    USE = 0x17,
-    USE_INP = 0x18,
-    RUN_TAPE = 0x19,
-    JOIN_TAPE = 0x1A,
-    CRASH = 0x1B,
-    USE_PREP = 0x1C,
-    STARTGRIND = 0x1D,
-    STOPGRIND = 0x1E,
-    NPLAYERS = 0xE2,
-    THRESHOLD = 0xE3,
-    PLAYERID = 0xE4,
-    USE_EDABIT = 0xE5,
+    LDTN(InstructionOpcode, RegisterAddr),
+    LDARG(InstructionOpcode, RegisterAddr),
+    REQBL(InstructionOpcode, ImmediateValue),
+    STARG(InstructionOpcode, RegisterAddr),
+    TIME(InstructionOpcode),
+    STARTTIMER(InstructionOpcode, ImmediateValue),
+    STOPTIMER(InstructionOpcode, ImmediateValue),
+    USE(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    USE_INP(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    RUN_TAPE(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr, RegisterAddr),
+    JOIN_TAPE(InstructionOpcode, RegisterAddr),
+    CRASH(InstructionOpcode, RegisterAddr),
+    NPLAYERS(InstructionOpcode, RegisterAddr),
+    THRESHOLD(InstructionOpcode, RegisterAddr),
+    PLAYERID(InstructionOpcode, RegisterAddr),
+    USE_EDABIT(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
     /// Addition
-    ADDC = 0x20,
-    ADDS = 0x21,
-    ADDM = 0x22,
-    ADDCI = 0x23,
-    ADDSI = 0x24,
-    SUBC = 0x25,
-    SUBS = 0x26,
-    SUBML = 0x27,
-    SUBMR = 0x28,
-    SUBCI = 0x29,
-    SUBSI = 0x2A,
-    SUBCFI = 0x2B,
-    SUBSFI = 0x2C,
+    ADDC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    ADDS(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    ADDM(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    ADDCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    ADDSI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    SUBC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    SUBS(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    SUBML(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    SUBMR(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    SUBCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    SUBSI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    SUBCFI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    SUBSFI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
     /// Multiplication/division
-    MULC = 0x30,
-    MULM = 0x31,
-    MULCI = 0x32,
-    MULSI = 0x33,
-    DIVC = 0x34,
-    DIVCI = 0x35,
-    MODC = 0x36,
-    MODCI = 0x37,
-    LEGENDREC = 0x38,
-    DIGESTC = 0x39,
-    INV2M = 0x3a,
-    FLOORDIVC = 0x3b,
-    GMULBITC = 0x136,
-    GMULBITM = 0x137,
+    MULC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    MULM(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    MULCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    MULSI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    DIVC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    DIVCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    MODC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    MODCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    LEGENDREC(InstructionOpcode, RegisterAddr, RegisterAddr),
+    DIGESTC(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    INV2M(InstructionOpcode, RegisterAddr, ImmediateValue),
+    FLOORDIVC(InstructionOpcode, RegisterAddr, RegisterAddr),
     /// Open
-    OPEN = 0xA5,
-    MULS = 0xA6,
-    MULRS = 0xA7,
-    DOTPRODS = 0xA8,
-    TRUNC_PR = 0xA9,
-    MATMULS = 0xAA,
-    MATMULSM = 0xAB,
-    CONV2DS = 0xAC,
-    CHECK = 0xAF,
+    // TODO: Figure out sensible way of handling variable length arguments
+    MULS(InstructionOpcode, ImmediateValue, RegisterAddr, RegisterAddr, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    MULRS(InstructionOpcode, ImmediateValue, ImmediateValue, RegisterAddr, RegisterAddr, RegisterAddr),
+    // TODO: Minimize parameters
+    // TODO: Figure out sensible way of handling variable length arguments
+    DOTPRODS(InstructionOpcode, ImmediateValue, ImmediateValue, RegisterAddr, RegisterAddr, RegisterAddr, RegisterAddr),
+    TRUNC_PR(InstructionOpcode, ImmediateValue, RegisterAddr, RegisterAddr, ImmediateValue, ImmediateValue),
+    MATMULS(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue, ImmediateValue),
+    MATMULSM(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr, ImmediateValue, ImmediateValue, ImmediateValue, RegisterAddr, RegisterAddr, RegisterAddr, RegisterAddr, ImmediateValue, ImmediateValue),
+    CHECK(InstructionOpcode),
     /// Data access
-    TRIPLE = 0x50,
-    BIT = 0x51,
-    SQUARE = 0x52,
-    INV = 0x53,
-    GBITTRIPLE = 0x154,
-    GBITGF2NTRIPLE = 0x155,
-    INPUTMASK = 0x56,
-    INPUTMASKREG = 0x5C,
-    PREP = 0x57,
-    DABIT = 0x58,
-    EDABIT = 0x59,
-    SEDABIT = 0x5A,
-    RANDOMS = 0x5B,
-    RANDOMFULLS = 0x5D,
+    TRIPLE(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    BIT(InstructionOpcode, RegisterAddr),
+    SQUARE(InstructionOpcode, RegisterAddr, RegisterAddr),
+    INV(InstructionOpcode, RegisterAddr, RegisterAddr),
+    INPUTMASK(InstructionOpcode, RegisterAddr, ImmediateValue),
+    INPUTMASKREG(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    DABIT(InstructionOpcode, RegisterAddr, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    EDABIT(InstructionOpcode, ImmediateValue, RegisterAddr, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    SEDABIT(InstructionOpcode, ImmediateValue, RegisterAddr, RegisterAddr),
+    RANDOMS(InstructionOpcode, RegisterAddr, ImmediateValue),
+    RANDOMFULLS(InstructionOpcode, RegisterAddr),
     /// Input
-    INPUT = 0x60,
-    INPUTFIX = 0xF0,
-    INPUTFLOAT = 0xF1,
-    INPUTMIXED = 0xF2,
-    INPUTMIXEDREG = 0xF3,
-    RAWINPUT = 0xF4,
-    INPUTPERSONAL = 0xF5,
-    STARTIPUT = 0x61,
-    STOPINPUT = 0x62,
-    READSOCKETC = 0x63,
-    READSOCKETS = 0x64,
-    WRITESOCKETC = 0x65,
-    WRITESOCKETS = 0x66,
-    READSOCKETINT = 0x69,
-    WRITESOCKETINT = 0x6a,
-    WRITESOCKETSHARE = 0x6b,
-    LISTEN = 0x6c,
-    ACCEPTCLIENTCONNECTION = 0x6d,
-    CLOSECLIENTCONNECTION = 0x6e,
-    READCLIENTPUBLICKEY = 0x6f,
+    /// May not be needed INPUT = 0x60,
+    /// INPUTFIX = 0xF0,
+    /// INPUTFLOAT = 0xF1,
+    INPUTMIXED(InstructionOpcode, ImmediateValue, RegisterAddr, Option<RegisterAddr>, Option<RegisterAddr>, Option<RegisterAddr>, Option<ImmediateValue>, ImmediateValue),
+    /// INPUTMIXEDREG = 0xF3,
+    RAWINPUT(InstructionOpcode, ImmediateValue, ImmediateValue, RegisterAddr),
+    /// May not be needed INPUTPERSONAL = 0xF5,
+    // TODO: Figure out sensible way of handling variable length arguments
+    READSOCKETC(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    READSOCKETS(InstructionOpcode, ImmediateValue, ImmediateValue, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    WRITESOCKETC(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    WRITESOCKETS(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    READSOCKETINT(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue, ImmediateValue, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    WRITESOCKETINT(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    WRITESOCKETSHARE(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue, RegisterAddr),
+    LISTEN(InstructionOpcode, RegisterAddr),
+    ACCEPTCLIENTCONNECTION(InstructionOpcode, RegisterAddr, RegisterAddr),
+    CLOSECLIENTCONNECTION(InstructionOpcode, RegisterAddr),
     /// Bitwise logic
-    ANDC = 0x70,
-    XORC = 0x71,
-    ORC = 0x72,
-    ANDCI = 0x73,
-    XORCI = 0x74,
-    ORCI = 0x75,
-    NOTC = 0x76,
+    ANDC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    XORC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    ORC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    ANDCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    XORCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    ORCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    NOTC(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
     /// Bitwise shifts
-    SHLC = 0x80,
-    SHRC = 0x81,
-    SHLCI = 0x82,
-    SHRCI = 0x83,
-    SHRSI = 0x84,
+    SHLC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    SHRC(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    SHLCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    SHRCI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    SHRSI(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
     /// Branching and comparison
-    JMP = 0x90,
-    JMPNZ = 0x91,
-    JMPEQZ = 0x92,
-    EQZC = 0x93,
-    LTZC = 0x94,
-    LTC = 0x95,
-    GTC = 0x96,
-    EQC = 0x97,
-    JMPI = 0x98,
+    JMP(InstructionOpcode, ImmediateValue),
+    JMPNZ(InstructionOpcode, ImmediateValue, ImmediateValue),
+    JMPEQZ(InstructionOpcode, ImmediateValue, ImmediateValue),
+    EQZC(InstructionOpcode, ImmediateValue, ImmediateValue),
+    LTZC(InstructionOpcode, ImmediateValue, ImmediateValue),
+    LTC(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue),
+    GTC(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue),
+    EQC(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue),
+    JMPI(InstructionOpcode, ImmediateValue),
     /// Integers
-    BITDECINT = 0x99,
-    LDINT = 0x9A,
-    ADDINT = 0x9B,
-    SUBINT = 0x9C,
-    MULINT = 0x9D,
-    DIVINT = 0x9E,
-    PRINTINT = 0x9F,
-    INCINT = 0xD1,
-    SHUFFLE = 0xD2,
+    // TODO: Figure out sensible way of handling variable length arguments
+    BITDECINT(InstructionOpcode, ImmediateValue, RegisterAddr, RegisterAddr, RegisterAddr),
+    LDINT(InstructionOpcode, RegisterAddr, ImmediateValue),
+    ADDINT(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    SUBINT(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    MULINT(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    DIVINT(InstructionOpcode, RegisterAddr, RegisterAddr, RegisterAddr),
+    /// Do we need print instructions?
+    /// PRINTINT(InstructionOpcode, RegisterAddr),
+    INCINT(InstructionOpcode, ImmediateValue, ImmediateValue, ImmediateValue, ImmediateValue, ImmediateValue),
+    SHUFFLE(InstructionOpcode, RegisterAddr, RegisterAddr),
     /// Conversion
-    CONVINT = 0xC0,
-    CONVMODP = 0xC1,
-    GCONVGF2N = 0x1C1,
+    CONVINT(InstructionOpcode, RegisterAddr, RegisterAddr),
+    CONVMODP(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    GCONVGF2N(InstructionOpcode, RegisterAddr, RegisterAddr),
     /// IO
-    PRINTMEM = 0xB0,
-    PRINTREG = 0xB1,
-    RAND = 0xB2,
-    PRINTREGPLAIN = 0xB3,
-    PRINTCHR = 0xB4,
-    PRINTSTR = 0xB5,
-    PUBINPUT = 0xB6,
-    RAWOUTPUT = 0xB7,
-    STARTPRIVATEOUTPUT = 0xB8,
-    STOPPRIVATEOUTPUT = 0xB9,
-    PRINTCHRINT = 0xBA,
-    PRINTSTRINT = 0xBB,
-    PRINTFLOATPLAIN = 0xBC,
-    WRITEFILESHARE = 0xBD,
-    READFILESHARE = 0xBE,
-    CONDPRINTSTR = 0xBF,
-    PRINTFLOATPREC = 0xE0,
-    CONDPRINTPLAIN = 0xE1,
-    INTOUTPUT = 0xE6,
-    FLOATOUTPUT = 0xE7,
-    GBITDEC = 0x184,
-    GBITCOM = 0x185,
-    /// Secure socket
-    INITSECURESOCKET() = 0x1BA,
-    RESPSECURESOCKET = 0x1BB,
+    /// Do we need print instructions?
+    /// PRINTREG = 0xB1,
+    RAND(InstructionOpcode, RegisterAddr, RegisterAddr),
+    /// PRINTREGPLAIN = 0xB3,
+    /// PRINTCHR = 0xB4,
+    /// PRINTSTR = 0xB5,
+    PUBINPUT(InstructionOpcode, RegisterAddr),
+    RAWOUTPUT(InstructionOpcode, RegisterAddr),
+    STARTPRIVATEOUTPUT(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    STOPPRIVATEOUTPUT(InstructionOpcode, RegisterAddr, RegisterAddr, ImmediateValue),
+    /// PRINTFLOATPLAIN = 0xBC,
+    // TODO: Figure out sensible way of handling variable length arguments
+    WRITEFILESHARE(InstructionOpcode, ImmediateValue, ImmediateValue, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    READFILESHARE(InstructionOpcode, ImmediateValue, ImmediateValue, RegisterAddr, RegisterAddr),
+    /// CONDPRINTSTR = 0xBF,
+    /// PRINTFLOATPREC = 0xE0,
+    /// CONDPRINTPLAIN = 0xE1,
+    INTOUTPUT(InstructionOpcode, ImmediateValue, RegisterAddr),
+    FLOATOUTPUT(InstructionOpcode, ImmediateValue, RegisterAddr, RegisterAddr, RegisterAddr, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    GBITDEC(InstructionOpcode, RegisterAddr, ImmediateValue, RegisterAddr),
+    // TODO: Figure out sensible way of handling variable length arguments
+    GBITCOM(InstructionOpcode, RegisterAddr, ImmediateValue, RegisterAddr),
+}
+
+impl Opcode {
+
 }
