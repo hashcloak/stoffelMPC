ea1ffdc4ab62f1fff4cc6e52c626806a5da21201
Mikerah <mikerah@hashcloak.com>
2022-07-08 18:17:46 -0400

    Added more vm specific types to types create

--- types/src/vm/mod.rs
+++ types/src/vm/mod.rs
@@ -1,57 +1,12 @@
-use std::ops::{Add, Mul};
-
 pub mod ark;
-
 pub mod number;
 
-/// This trait is a catch-all for a type that is used
-/// within the virtual machine
-pub trait Number:
-    Add<Output = Self> + Mul<Output = Self> + Copy + Default + std::fmt::Debug + Sized + 'static
-{
-    /// Add two numbers
-    ///
-    /// Use the implementation provided by the implementor
-    fn add(self, b: Self) -> Self {
-        Add::add(self, b)
-    }
+pub use ark::*;
+pub use number::*;
 
-    /// Multiply two numbers
-    ///
-    /// Use the implementation provided by the implementor
-    fn mul(self, b: Self) -> Self {
-        Mul::mul(self, b)
-    }
+/// Register address type for indicating which register to use
+pub type RegisterAddr = usize;
 
-    /// Returns the square of an MPCType
-    fn square(self) -> Self {
-        todo!();
-    }
-
-    /// Returns the power of itself to an exponent
-    fn pow(self, exp: usize) -> Self {
-        todo!();
-    }
-
-    /// Returns the minimum between itself and another MPCType
-    fn min(self, a: Self) -> Self {
-        todo!();
-    }
-
-    /// Returns the maximum between itself and another MPCType
-    fn max(self, b: Self) -> Self {
-        todo!();
-    }
-
-    /// Returns a if self == 1 and b if self == 0
-    fn if_else(self, a: Self, b: Self) -> Self {
-        todo!();
-    }
-
-    /// Returns (a, b) if self == 0 and (b, a) if self == 1
-    fn cond_swap(self, a: Self, b: Self) -> (Self, Self) {
-        todo!();
-    }
-}
-
-impl Number for u32 {}
+/// Immediate value type for clear values
+/// TODO: Change this to be of a specific Number type instead of u64
+pub type ImmediateValue = u64;
